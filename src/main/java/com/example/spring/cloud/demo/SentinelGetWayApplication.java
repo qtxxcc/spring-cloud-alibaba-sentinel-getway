6d1eece02461cef2cdf053076c98c08ceb123017
wanghanbin <wanghanbin123456>
2020-04-01 10:24:28 +0800

    初始化spring-cloud-alibaba-sentinel-getway

---
+++ src/main/java/com/example/spring/cloud/demo/SentinelGetWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelGetWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelGetWayApplication.class, args);
+    }
+
+}
